8017557a3a3dd4af6522679f3b40c3632d24e9dc
mbataprecious <mbataprecious9@gmail.com>
2023-04-29 09:20:57 +0100

    addressed mail sending

--- helpers/sendVerificationMail.js
+++ helpers/sendVerificationMail.js
@@ -8,30 +8,31 @@ console.log(
   config.email,
   config.username
 );
-// let transporter = nodemailer.createTransport({
-//   host: config.mailServer, //replace with your email provider
-//   port: Number(config.smtpPort),
-//   tls: true,
-//   auth: {
-//     user: config.username,
-//     pass: config.password,
-//   },
-// });
+let transporter = nodemailer.createTransport({
+  host: config.mailServer, //replace with your email provider
+  port: Number(config.smtpPort),
+  tls: true,
+  auth: {
+    user: config.username,
+    pass: config.password,
+  },
+});
 
-const sparkPostTransport = require("nodemailer-sparkpost-transport"),
-  transporter = nodemailer.createTransport(
-    sparkPostTransport({
-      sparkPostApiKey: "933afab3231d67f6e8dbf7721fd656551d707ab7",
-      options: {
-        open_tracking: true,
-        click_tracking: true,
-        transactional: true,
-      },
-    })
-  );
+// const sparkPostTransport = require("nodemailer-sparkpost-transport"),
+//   transporter = nodemailer.createTransport(
+//     sparkPostTransport({
+//       sparkPostApiKey: "933afab3231d67f6e8dbf7721fd656551d707ab7",
+//       options: {
+//         open_tracking: true,
+//         click_tracking: true,
+//         transactional: true,
+//       },
+//     })
+//   );
 
 // verify connection configuration
 transporter.verify(function (error, success) {
+  console.log("transporter initiated");
   if (error) {
     console.log(error.message, "verification error");
   } else {
